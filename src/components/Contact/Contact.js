a828b7dfe00aa1d238cd17707137f227d0b9f482
ridhobenderaku <ridhobenderaku@gmail.com>
2024-01-24 20:40:03 +0700

    cv

---
+++ src/components/Contact/Contact.js
@@ -0,0 +1,102 @@
+import React, { useState } from "react";
+import { ContactWrapper, Email } from "./ContactElements";
+import{WA} from "./WaElement";
+import { MdContentCopy } from "react-icons/md";
+import { IconButton, Tooltip } from "@mui/material";
+import Zoom from '@mui/material/Zoom';
+
+import ScrollAnimation from "react-animate-on-scroll";
+function Contact() {
+  const [showTooltip, setShowTooltip] = useState(false);
+  const [showToolWa, setShowToolWa] = useState(false);
+  const copyToClipboard = () => {
+    navigator.clipboard.writeText("muhridho448@gmail.com");
+    setShowTooltip(true);
+    setTimeout(() => {
+      setShowTooltip(false);
+    }, 700);
+  };
+  const copyToWA = () => {
+    navigator.clipboard.writeText("6281249429071");
+    setShowToolWa(true);
+    setTimeout(() => {
+      setShowToolWa(false);
+    }, 700);
+  };
+
+  return (
+    <ContactWrapper id="contact">
+
+      <div className="Container">
+        <div className="SectionTitle">Get In Touch</div>
+        <ScrollAnimation animateIn="fadeIn" >
+          <div className="BigCard">
+          <Email>
+              <div style={{ display: 'flex', alignItems: 'center', columnGap: '20px', rowGap: '10px', flexWrap: 'wrap', justifyContent: 'center' }} >
+                <span>muhridho448@gmail.com</span>
+                <Tooltip
+                  PopperProps={{
+                    disablePortal: true,
+                  }}
+                  open={showTooltip}
+                  onClose={() => setShowTooltip(false)}
+                  title="Copied!"
+                  TransitionComponent={Zoom}
+                  disableFocusListener
+                  disableHoverListener
+                  disableTouchListener
+                  placement="bottom"
+                >
+                  <IconButton  onClick={copyToClipboard} >
+                    <MdContentCopy size={25} style={{ cursor: 'pointer', color: "#151418" }}/>
+                  </IconButton>
+                </Tooltip>
+              </div>
+              <a
+                className="btn PrimaryBtn btn-shadow"
+                href="mailto:muhridho448@gmail.com"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Send Email
+              </a>
+            </Email>
+            <WA>
+            <div style={{ display: 'flex',textAlign:'right', alignItems: 'right',columnGap: '120px', justifyContent: 'right', position:'relative' }} >
+             <span> +62 81249429071          </span>
+                <Tooltip
+                  PopperProps={{
+                    disablePortal: true,
+                  }}
+                  open={showToolWa}
+                  onClose={() => setShowToolWa(false)}
+                  title="Copied!"
+                  TransitionComponent={Zoom}
+                  disableFocusListener
+                  disableHoverListener
+                  disableTouchListener
+                  placement="bottom"
+                >
+                  <IconButton  onClick={copyToWA} >
+                    <MdContentCopy size={25} style={{ cursor: 'pointer', color: "#151418" }}/>
+                  </IconButton>
+                </Tooltip>
+                </div>
+                <a
+                className="btn SuccessBtn btn-shadow"
+                  target="_blank"
+                  href="https://api.whatsapp.com/send?phone=6281249429071"
+                >
+                  Contact Us
+                </a>
+              </WA>
+
+          </div>
+        </ScrollAnimation>
+
+      </div>
+    </ContactWrapper>
+  );
+}
+
+export default Contact;
---
+++ src/data/ProjectData.js
@@ -0,0 +1,121 @@
+// --------------------------------------- Project List
+export const ProjectList = [
+  {
+    img: "/portofolio/project.png",
+    title: "Ventura Transport",
+    description:
+      "Our passion is in providing transportation services for you. We are sure to guarantee your comfort and safety when driving to your destination safely.",
+    tech_stack: ["Next Js", "Google Cloud", "TailwindCSS"],
+    github_url: "",
+    demo_url: "",
+  },
+  {
+    img: "/renr-responsive.png",
+    title: "RENR Safety App",
+    description:
+      "A safety application for the Renewable Resources Dept. of BCIT",
+    tech_stack: ["React", "Firebase"],
+    github_url: "",
+    demo_url: "",
+  },
+  {
+    img: "/mymind-responsive.png",
+    title: "MyMind",
+    description:
+      "An app where users can purchase online therapy sessions and chat with their therapists.",
+    tech_stack: ["HTML", "CSS", "NodeJs", "MongoDB", "SocketIo"],
+    github_url: "",
+    demo_url: "",
+  },
+  {
+    img: "/pokedex-responsive.png",
+    title: "Pokedex",
+    description:
+      "A pokedex app that displays pokemon data, plays OSTs and makes pokemons dance.",
+    tech_stack: ["ReactJs", "NodeJs", "MongoDB"],
+    github_url: "",
+    demo_url: "",
+  },
+  {
+    img: "/jobDetect.png",
+    title: "JobDetect",
+    description:
+      "An app to detect if a job posting is a scam or not using its description.",
+    tech_stack: ["Python", "Flask", "NodeJs", "MongoDB"],
+    github_url: "",
+    demo_url: "",
+  },
+];
+
+// --------------------------------------- Skills
+
+export const stackList = [
+  {
+    img: "/icone/html5.svg",
+    name: "HTML",
+  },
+  {
+    img: "/icone/css.svg",
+    name: "CSS",
+  },
+  {
+    img: "/icone/javascript.svg",
+    name: "JavaScript",
+  },
+  {
+    img: "/icone/jquery.svg",
+    name: "Jquery",
+  },
+  {
+    img: "/icone/react.svg",
+    name: "ReactJS",
+  },
+  {
+    img: "/icone/nextjs.svg",
+    name: "NextJs",
+  },
+  {
+    img: "/icone/typescript.svg",
+    name: "Typescript",
+  },
+  {
+    img: "/icone/git.svg",
+    name: "Git",
+  },
+  {
+    img: "/icone/nodejs.svg",
+    name: "NodeJS",
+  },
+  {
+    img: "/icone/php.svg",
+    name: "PHP",
+  },
+  {
+    img: "/icone/ci3.svg",
+    name: "CodeIgniter",
+  },
+  {
+    img: "/icone/laravel.svg",
+    name: "Laravel",
+  },
+  {
+    img: "icone/mysql.svg",
+    name: "MySql",
+  },
+  {
+    img: "/icone/bootstrap.svg",
+    name: "Bootstrap",
+  },
+  {
+    img: "/icone/materialui.svg",
+    name: "Material UI",
+  },
+  {
+    img: "/icone/tailwindcss.svg",
+    name: "TailwindCSS",
+  },
+  {
+    img: "/icone/vscode.svg",
+    name: "VS Code",
+  },
+];
