c210d2d207e3d9103b9d420fb139c347488a73d8
ridho813 <muhridho448@gmail.com>
2026-03-31 09:10:15 +0700

    Uploadf

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,14 +1,13 @@
 import React, { useState } from "react";
 import { ContactWrapper, Email } from "./ContactElements";
-import{WA} from "./WaElement";
 import { MdContentCopy } from "react-icons/md";
 import { IconButton, Tooltip } from "@mui/material";
-import Zoom from '@mui/material/Zoom';
-import { AnimationOnScroll } from 'react-animation-on-scroll';
+import Zoom from "@mui/material/Zoom";
+import { AnimationOnScroll } from "react-animation-on-scroll";
 
 function Contact() {
   const [showTooltip, setShowTooltip] = useState(false);
-  const [showToolWa, setShowToolWa] = useState(false);
+
   const copyToClipboard = () => {
     navigator.clipboard.writeText("muhridho448@gmail.com");
     setShowTooltip(true);
@@ -16,27 +15,27 @@ function Contact() {
       setShowTooltip(false);
     }, 700);
   };
-  const copyToWA = () => {
-    navigator.clipboard.writeText("6281249429071");
-    setShowToolWa(true);
-    setTimeout(() => {
-      setShowToolWa(false);
-    }, 700);
-  };
 
   return (
     <ContactWrapper id="contact">
       <div className="Container">
         <div className="SectionTitle">Get In Touch</div>
-        <AnimationOnScroll animateIn="fadeIn" >
+        <AnimationOnScroll animateIn="fadeIn">
           <div className="BigCard">
-          <Email>
-              <div style={{ display: 'flex', alignItems: 'center', columnGap: '20px', rowGap: '10px', flexWrap: 'wrap', justifyContent: 'center' }} >
+            <Email>
+              <div
+                style={{
+                  display: "flex",
+                  alignItems: "center",
+                  columnGap: "20px",
+                  rowGap: "10px",
+                  flexWrap: "wrap",
+                  justifyContent: "center",
+                }}
+              >
                 <span>muhridho448@gmail.com</span>
+
                 <Tooltip
-                  PopperProps={{
-                    disablePortal: true,
-                  }}
                   open={showTooltip}
                   onClose={() => setShowTooltip(false)}
                   title="Copied!"
@@ -46,11 +45,15 @@ function Contact() {
                   disableTouchListener
                   placement="bottom"
                 >
-                  <IconButton  onClick={copyToClipboard} >
-                    <MdContentCopy size={25} style={{ cursor: 'pointer', color: "#151418" }}/>
+                  <IconButton onClick={copyToClipboard}>
+                    <MdContentCopy
+                      size={25}
+                      style={{ cursor: "pointer", color: "#151418" }}
+                    />
                   </IconButton>
                 </Tooltip>
               </div>
+
               <a
                 className="btn PrimaryBtn btn-shadow"
                 href="mailto:muhridho448@gmail.com"
